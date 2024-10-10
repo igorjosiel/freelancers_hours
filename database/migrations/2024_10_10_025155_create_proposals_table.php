fce2bb945bc5d5c43944dd36846f3b07fd3949c9
Igor Josiel <igorjosielvitalino@gmail.com>
2024-10-10 09:16:21 -0300

    Criação das tabelas do banco com suas estruturas e registros.

--- database/migrations/2024_10_10_025155_create_proposals_table.php
+++ database/migrations/2024_10_10_025155_create_proposals_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Project;
 
 return new class extends Migration
 {
@@ -11,10 +12,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('proposals', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->unsignedSmallInteger('hours');
+            $table->foreignIdFor(Project::class)->constrained();
+            $table->timestamps();
         });
     }
 
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('proposals');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Project;
+use App\Models\Proposal;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->count(200)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::query()->inRandomOrder()->limit(10)->get()
+            ->each(function (User $user) {
+                $project = Project::factory()->create(['created_by' => $user->id]);
+
+                Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+            });
     }
 }
